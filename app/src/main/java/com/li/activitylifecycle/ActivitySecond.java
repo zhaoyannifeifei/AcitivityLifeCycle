bdf3f178a801966d54cb74613f8b430bfec300a9
lsx <lsx@163.com>
2016-07-22 14:37:50 +0800

    可以打开activitysecond

---
+++ app/src/main/java/com/li/activitylifecycle/ActivitySecond.java
@@ -0,0 +1,9 @@
+package com.li.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class ActivitySecond extends AppCompatActivity {
+}
